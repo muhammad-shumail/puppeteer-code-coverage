c13f59dc7c9c8b065e36bd1b7aa96d633cdbaf1d
Muhammad Shumail <thinktechnologies94@gmail.com>
2023-12-04 03:22:45 +0500

    Remove unnecessary files and code

--- code_coverage.js
+++ code_coverage.js
@@ -1,32 +1,3 @@
-/**
- * Copyright 2018 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @author ebidel@ (Eric Bidelman)
- */
-
-/**
- * Shows how to use Puppeteer's code coverage API to measure CSS/JS coverage across
- * different points of time during loading. Great for determining if a lazy loading strategy
- * is paying off or working correctly.
- *
- * Install:
- *   npm i puppeteer chalk cli-table
- * Run:
- *   URL=https://example.com node code_coverage.js
- */
-
 const puppeteer = require('puppeteer');
 const chalk = require('chalk');
 const Table = require('cli-table');
@@ -38,7 +9,7 @@ const EVENTS = [
   'load',
   'networkidle2',
   'networkidle0',
-  
+
 ];
 
 function formatBytesToKB(bytes) {
